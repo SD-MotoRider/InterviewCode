fc059ab705f0a4640386d0334265628cf949439d
NA\msimpson <msimpson@qti.qualcomm.com>
2018-04-23 13:29:52 -0700

    Matrix template code

---
+++ Matrix/Matrix.h
@@ -0,0 +1,84 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+// MIT License
+//
+// Copyright (c) 2018 Michael Simpson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <algorithm>
+#include <memory.h>
+
+template<typename T> class Matrix
+{
+public:
+	Matrix() = delete; // No default constructor
+	~Matrix()
+	{
+		if (_array != nullptr)
+		{
+			_array = delete [];
+			_array = nullptr;
+		}
+	}
+
+	Matrix(const Matrix& copyMe)
+	{
+		_rows = copyMe._rows;
+		_cols = copyMe._cols;
+
+		unsigned long arrayCells(_rows * _cols);
+		_array =  new T[arrayCells];
+		if (_array != nullptr)
+		{
+			::memcpy(_array, copyMe._array, arrayCells * sizeof(T));
+		}
+	}
+
+	Matrix(unsigned short rows, unsigned short cols, T initialValue) :
+		_rows(rows),
+		_cols(cols)
+	{
+		unsigned long arrayCells(_rows * _cols); // large enough to hold USHRT_MAX * USHRT_MAX
+
+		if (arrayCells > 0) // make sure there is dimension
+		{
+			_array = new T[arrayCells];
+			if (_array != nullptr)
+			{
+				std::fill(_array, _array + arrayCells - 1, initialValue);
+			}
+		}
+	}
+
+	Matrix<T> transpose(void)
+	{
+		Matrix<T> result(2, 3, 0);
+
+		return result;
+	}
+
+private:
+	unsigned short				_rows;
+	unsigned short				_cols;
+	T*							_array;
+};
+
+#endif // MATRIX_H
